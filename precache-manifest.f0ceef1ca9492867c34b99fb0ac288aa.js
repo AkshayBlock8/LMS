c20bb5d1e85329a0fe4a3ea86d07866cf2958470
Akshay Kumar <email.akshay98@gmail.com>
2020-02-18 10:47:34 +0530

    Updates

---
+++ precache-manifest.f0ceef1ca9492867c34b99fb0ac288aa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cea5c8d55e43a5d21c2a32ec66883a81",
+    "url": "/LMS/index.html"
+  },
+  {
+    "revision": "2edb64e65da7be931c2b",
+    "url": "/LMS/static/css/main.629b102e.chunk.css"
+  },
+  {
+    "revision": "5e999263e0733872b77b",
+    "url": "/LMS/static/js/2.0f207427.chunk.js"
+  },
+  {
+    "revision": "8e206c6708675092ad76215856e3fa46",
+    "url": "/LMS/static/js/2.0f207427.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2edb64e65da7be931c2b",
+    "url": "/LMS/static/js/main.64897bfb.chunk.js"
+  },
+  {
+    "revision": "bcbf1e97ff3fab920fd4",
+    "url": "/LMS/static/js/runtime-main.c7525a5f.js"
+  }
+]);
