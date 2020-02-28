e55817b66a9eedfb5afe4a484546629217461455
Ekampreet Singh <ekampreet.singh@block8.com>
2020-02-28 12:14:51 +0530

    nodemailer dotenv email.js mailOptions

--- server/utils/email.js
+++ server/utils/email.js
@@ -1,4 +1,20 @@
-function emailservice() {
-    // -
+require('dotenv').config();
+const nodemailer = require("nodemailer");
+
+async function emailservice(mailOptions) {
+    console.log(mailOptions)
+    let transporter = nodemailer.createTransport({
+        service:'gmail',
+        auth:{
+            user: process.env.EMAIL,
+            pass: process.env.PASSWORD
+        }
+    });
+
+    let info = await transporter.sendMail(mailOptions);
+
+    console.log("Message sent: %s", info.messageId);
+    // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
 }
+
 exports.email=emailservice;
